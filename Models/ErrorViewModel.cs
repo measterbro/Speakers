49a243a555afe6921981286e3cb17e8fa6df42b5
Michael Easterbrook <measterbro@hotmail.com>
2022-04-05 12:04:56 -0400

    test of staging

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace Security2.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        Type type { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
